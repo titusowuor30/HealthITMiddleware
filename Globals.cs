7762fc035e21dd605a188ad9944a12d66278af8a
Titus Owuor <titusowuor30@gmail.com>
2022-06-11 09:45:00 +0300

    updates

--- Globals.cs
+++ Globals.cs
@@ -4,7 +4,7 @@ namespace HealthITMiddleware
     {
         public static string clientUrl = "http://212.49.81.13:4444/";   //http://localhost:44356/
         public static string serverUrl = "http://localhost:4444/";
-        public static string clientEmail = "titusowuor30@gmail.com";
+        public static string clientEmail = "admin@admin.com";
         public static string clientPassword = "@Admin123";
         public static string serveremail = "admin@admin.com";
         public static string serverpassword = "@Admin123";
